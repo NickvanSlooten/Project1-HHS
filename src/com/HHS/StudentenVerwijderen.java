c9bf577f3d9b3ff27257b9d1a5a53bb7ac7014fa
EmreDH <20150393@student.hhs.nl>
2022-03-14 15:29:11 +0100

    Create StudentenVerwijderen.java
    studenten verwijderen voorbeeld

---
+++ src/com/HHS/StudentenVerwijderen.java
@@ -0,0 +1,17 @@
+package com.HHS;
+
+import java.util.ArrayList;
+
+public class StudentenVerwijderen {
+
+public static void main (String[] args) {
+    ArrayList<String> namesStudent = new ArrayList<String>();
+    namesStudent.add("a");
+    namesStudent.add("b");
+    namesStudent.add("c");
+    namesStudent.remove(0);     // welke student je wilt verwijderen uit de ArrayList
+    System.out.println(namesStudent);
+
+            }
+       }
+       
