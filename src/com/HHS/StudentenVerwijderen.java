c075b9321849d9c53ca473b749a1e7a799645a73
EmreDH <20150393@student.hhs.nl>
2022-03-15 12:30:19 +0100

    AR remove
    arraylist remove student

--- src/com/HHS/StudentenVerwijderen.java
+++ src/com/HHS/StudentenVerwijderen.java
@@ -5,13 +5,12 @@ import java.util.ArrayList;
 public class StudentenVerwijderen {
 
 public static void main (String[] args) {
-    ArrayList<String> namesStudent = new ArrayList<String>();
-    namesStudent.add("a");
-    namesStudent.add("b");
-    namesStudent.add("c");
-    namesStudent.remove(0);     // welke student je wilt verwijderen uit de ArrayList
-    System.out.println(namesStudent);
+    ArrayList<String> students = new ArrayList<String>();
+    students.remove(0);       // welke student je wilt verwijderen uit de ArrayList
+    students.remove(1);
+    students.remove(2);
+    System.out.println(students);
 
             }
        }
-       
+
