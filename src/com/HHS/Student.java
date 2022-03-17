4d32a300ff985874b8159ecbe1e51b4ed0aa70ab
VIMAH <21022208@student.hhs.nl>
2022-03-17 14:48:52 +0100

    student tonen basis

--- src/com/HHS/Student.java
+++ src/com/HHS/Student.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 
 public class Student {
 
-    private static ArrayList<Student>students = new ArrayList<>();
-    private ArrayList<Exam>passedExam = new ArrayList<>();
+    private static ArrayList<Student> students = new ArrayList<>();
+    private ArrayList<Exam> passedExam = new ArrayList<>();
     private double number;
     private String name;
 
-    public Student(double number, String name){
+    public Student(double number, String name) {
         this.number = number;
         this.name = name;
         students.add(this);
@@ -31,23 +31,26 @@ public class Student {
         this.name = name;
     }
 
-    public static void removeStudent(Student student){
+    public static void removeStudent(Student student) {
 
     }
 
-    public static ArrayList<Student> getStudent(){
-        //Return student
-        return null;
+    public void getStudent() {
+
+        for (int i = 0; i < students.size(); i++) {
+            System.out.println(students.get(i));
+        }
 
     }
-    public String toString(){
+
+    public String toString() {
         //return alle info student
         return null;
 
     }
-    public boolean hasPassedExam(Exam exam){
+
+    public boolean hasPassedExam(Exam exam) {
         //Check if exam passed then true else false    
         return false;
     }
-    
 }
