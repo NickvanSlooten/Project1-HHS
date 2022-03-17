06148b800e64f4af5e8cea9b9000f85116c6356c
1040249 <nickfloor11@gmail.com>
2022-03-17 13:17:30 +0100

    fixed tabs

--- src/com/HHS/Main.java
+++ src/com/HHS/Main.java
@@ -19,15 +19,15 @@ public class Main {
 
     public static void mainMenu() {
 
-System.out.println("1) Lijst met examens");
-System.out.println("2) Lijst met studenten");
-System.out.println("3) Nieuwe student inschrijven");
-System.out.println("4) Student verwijderen");
-System.out.println("5) Examen afnemen");
-System.out.println("6) Is student geslaagd voor test?");
-System.out.println("7) Welke examens heeft student gehaald?");
-System.out.println("8) Welke student heeft de meeste examens gehaald?");
-System.out.println("0) Exit");
+    System.out.println("1) Lijst met examens");
+    System.out.println("2) Lijst met studenten");
+    System.out.println("3) Nieuwe student inschrijven");
+    System.out.println("4) Student verwijderen");
+    System.out.println("5) Examen afnemen");
+    System.out.println("6) Is student geslaagd voor test?");
+    System.out.println("7) Welke examens heeft student gehaald?");
+    System.out.println("8) Welke student heeft de meeste examens gehaald?");
+    System.out.println("0) Exit");
 
     try (Scanner scanner = new Scanner(System.in)) {
         Integer userChoice = scanner.nextInt();
