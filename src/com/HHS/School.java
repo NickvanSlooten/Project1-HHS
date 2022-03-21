1d0aea389b3ebec8135be1fb9b6312841b134402
TudorGrigoras <tgrigoras@outlook.com>
2022-03-21 14:31:09 +0100

    printExam functie die alle examens uitprint

--- src/com/HHS/School.java
+++ src/com/HHS/School.java
@@ -15,6 +15,14 @@ public class School {
         exams.remove(exam);
     }
 
+    public void printExams()
+    {
+        for(Exam e: exams)
+        {
+            System.out.println(e.getName());
+        }
+    }
+
     public Student addStudent(String name)
     {
         students.add(new Student(2100000, name));
