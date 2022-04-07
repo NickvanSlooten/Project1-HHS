070e80356a67763f6d5c21834cb30529995f4e75
1040249 <nickfloor11@gmail.com>
2022-04-07 16:32:43 +0200

    getStudentByInputTest gemaakt

--- src/com/HHS/SchoolTest.java
+++ src/com/HHS/SchoolTest.java
@@ -3,6 +3,7 @@ package com.HHS;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -24,16 +25,14 @@ public class SchoolTest {
         assertEquals(school.getStudents().get(0).getName(), "DeNaam");
     }
     @Test
-    public void menuAddExamTest()
-    {
+    public void menuAddExamTest() {
         // Arrange
         School school = new School();
         String data = "DeNaam\n";
         System.setIn(new ByteArrayInputStream(data.getBytes()));
 
         // Act
-        for (Exam e : school.getExams())
-        {
+        for (Exam e : school.getExams()) {
             school.removeExam(e);
         }
         // Assert
@@ -42,4 +41,26 @@ public class SchoolTest {
         assertEquals(school.getExams().size(), 1);
         assertEquals(school.getExams().get(0).getName(), "DeNaam");
     }
+
+
+
+    
+    @Test
+    public void getStudentByInputTest() {
+    School school = new School();
+
+
+        for (Student s : school.getStudents())
+        {
+            school.removeStudent(s);
+        }
+
+        Student student = school.addStudent("Jantje");
+        String studentnummer = String.format("%d\n",student.getNumber());
+        System.setIn(new ByteArrayInputStream(studentnummer.getBytes()));
+
+        Assert.assertEquals(school.getStudentByInput(),student);
+
+   }
+
 }
