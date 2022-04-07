c445fe19bb29829ae018d0116cb63d7de1ab16e4
lemario132 <101121262+lemario132@users.noreply.github.com>
2022-04-07 16:32:02 +0200

    Added test for mostPassedExams and menuRemoveStudent.

--- src/com/HHS/SchoolTest.java
+++ src/com/HHS/SchoolTest.java
@@ -13,6 +13,7 @@ public class SchoolTest {
         System.setIn(new ByteArrayInputStream(data.getBytes()));
 
         School school = new School();
+        // Remove all existing students for testing
         for (Student s : school.getStudents())
         {
             school.removeStudent(s);
@@ -23,5 +24,42 @@ public class SchoolTest {
         Assert.assertEquals(school.getStudents().get(0).getName(), "DeNaam");
     }
 
-    
+    @Test
+    public void menuRemoveStudent()
+    {
+        School school = new School();
+        // Remove all existing students for testing
+        for (Student s : school.getStudents())
+        {
+            school.removeStudent(s);
+        }
+        school.addStudent("Student Naam");
+        Assert.assertEquals(school.getStudents().size(), 1);
+        String data = String.format("%d\n", school.getStudents().get(0).getNumber());
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        school.menuRemoveStudent();
+        Assert.assertEquals(school.getStudents().size(), 0);
+    }
+
+    @Test
+    public void studentWithMostPassedExams()
+    {
+        School school = new School();
+        // Remove all existing students for testing
+        for (Student s : school.getStudents())
+        {
+            school.removeStudent(s);
+        }
+        Exam ex1 = school.addExam("Ex 1");
+        Exam ex2 = school.addExam("Ex 2");
+        Exam ex3 = school.addExam("Ex 3");
+        school.addStudent("Student 1");
+        school.addStudent("Student 2").addPassedExam(ex1);
+        Student s3 = school.addStudent("Student 3/ de meeste");
+        s3.addPassedExam(ex1);
+        s3.addPassedExam(ex2);
+        s3.addPassedExam(ex3);
+        school.addStudent("Student 4");
+        Assert.assertEquals(school.studentWithMostPassedExams(), s3);
+    }
 }
